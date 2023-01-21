9bdecc956b7b18182f69c478d973851afc3ad4d1
Rupesh Shrestha <53545877+rupesh110@users.noreply.github.com>
2023-01-21 13:46:25 +1100

    Add files via upload

---
+++ cmPmDdTa/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <div className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuZmyVbPwdMmRcAK4cmqF70UCzM7o38SQ09A&usqp=CAU" className="header--img"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header-project">React Course - Project 3</h4>
+        </div>
+    )
+}
